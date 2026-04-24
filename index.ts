93c81e3c1d23b91ea56387a771c3f4276068edd2
axelldev <axellsolisrubio@gmail.com>
2026-04-23 19:56:45 -0600

    feat: handle send prompt when click send button

--- index.ts
+++ index.ts
@@ -3,8 +3,10 @@ import { type ChatCompletionMessageParam } from "openai/resources/chat/completio
 import { getConfig, checkEnvironment } from "./utils";
 
 const responseOutput = document.querySelector<HTMLElement>("[data-response]");
-if (!responseOutput) {
-  throw new Error("Response output element not found");
+const sendButton = document.querySelector<HTMLButtonElement>("#sendButton");
+
+if (!responseOutput || !sendButton) {
+  throw new Error("Response output or send button element not found");
 }
 
 const config = getConfig();
@@ -13,6 +15,7 @@ checkEnvironment(config);
 const openai = new OpenAI({
   apiKey: config.apiKey,
   baseURL: config.apiUrl,
+  dangerouslyAllowBrowser: true,
 });
 
 const messages: ChatCompletionMessageParam[] = [
@@ -22,9 +25,11 @@ const messages: ChatCompletionMessageParam[] = [
   },
 ];
 
-const response = await openai.chat.completions.create({
-  model: config.model,
-  messages,
+sendButton.addEventListener("click", async () => {
+  responseOutput.textContent = "Loading...";
+  const response = await openai.chat.completions.create({
+    model: config.model,
+    messages,
+  });
+  responseOutput.textContent = response.choices[0]?.message.content ?? "";
 });
-
-responseOutput.textContent = response.choices[0]?.message.content ?? "";
